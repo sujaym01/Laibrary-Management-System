45ccad9bb0ba90cb92b9dd8d9332c91812c47270
Sujay Mondal <sujay.m6160@gmail.com>
2024-02-22 13:21:33 +0600

    changes code

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = [' https://laibrary-management-system.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://laibrary-management-system.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
